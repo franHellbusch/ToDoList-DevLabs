adeb36650cc3cd04f38ba236da5cd4c993cf61a5
franHellbusch <fghellbusch@gmail.com>
2025-01-23 16:35:03 -0300

    fix/ changing forntend config for vercel routes and fixing margins for mobile

--- frontend/src/pages/private/Home/components/TaskList/components/TaskItem/components/TaskItemCollapse/TaskItemCollapse.tsx
+++ frontend/src/pages/private/Home/components/TaskList/components/TaskItem/components/TaskItemCollapse/TaskItemCollapse.tsx
@@ -15,45 +15,42 @@ interface ITaskItemCollapseProps {
   expanded: boolean;
 }
 
-const TaskItemCollapse: React.FC<ITaskItemCollapseProps> = ({
-  task,
-  expanded,
-}) => {
+const TaskItemCollapse: React.FC<ITaskItemCollapseProps> = ({ task, expanded }) => {
   return (
-    <Collapse in={expanded} timeout="auto" unmountOnExit>
-      <Box display="flex" flexWrap="wrap" gap="20px" marginTop="20px">
-        <Box display="flex" flexDirection="column" gap="5px" flex="1">
-          <TaskCollapseFieldTitle variant="h6" component="h5">
-            <CreateNewFolderRoundedIcon fontSize="small" />
+    <Collapse in={expanded} timeout='auto' unmountOnExit>
+      <Box display='flex' flexWrap='wrap' gap='20px' marginTop='20px'>
+        <Box display='flex' flexDirection='column' gap='5px' flex='1' minWidth="100px">
+          <TaskCollapseFieldTitle variant='h6' component='h5'>
+            <CreateNewFolderRoundedIcon fontSize='small' />
             Created At:
           </TaskCollapseFieldTitle>
-          <Typography variant="body1" component="p">
+          <Typography variant='body1' component='p'>
             {dayjs(task.createdAt).format("YYYY-MM-DD ")}
           </Typography>
         </Box>
-        <Box display="flex" flexDirection="column" gap="5px" flex="1">
-          <TaskCollapseFieldTitle variant="h6" component="h5">
-            <AccessAlarmRoundedIcon fontSize="small" />
+        <Box display='flex' flexDirection='column' gap='5px' flex='1' minWidth="100px">
+          <TaskCollapseFieldTitle variant='h6' component='h5'>
+            <AccessAlarmRoundedIcon fontSize='small' />
             Due Date:
           </TaskCollapseFieldTitle>
-          <Box display="flex" gap="5px" alignItems="center">
-            <Typography variant="body1" component="p">
+          <Box display='flex' gap='5px' alignItems='center'>
+            <Typography variant='body1' component='p'>
               {dayjs(task.dueDate).format("YYYY-MM-DD ")}
             </Typography>
             {dayjs(task.dueDate).isBefore(dayjs(), "day") && (
               <Chip
                 icon={<ReportGmailerrorredRoundedIcon />}
-                label="expired"
-                variant="outlined"
-                color="error"
-                size="small"
+                label='expired'
+                variant='outlined'
+                color='error'
+                size='small'
               />
             )}
           </Box>
         </Box>
-        <Box display="flex" flexDirection="column" gap="5px" flex="1">
-          <TaskCollapseFieldTitle variant="h6" component="h5">
-            <ReportGmailerrorredRoundedIcon fontSize="small" />
+        <Box display='flex' flexDirection='column' gap='5px' flex='1' minWidth="100px">
+          <TaskCollapseFieldTitle variant='h6' component='h5'>
+            <ReportGmailerrorredRoundedIcon fontSize='small' />
             Priority:
           </TaskCollapseFieldTitle>
           <Box>
@@ -61,38 +58,38 @@ const TaskItemCollapse: React.FC<ITaskItemCollapseProps> = ({
               <Chip
                 icon={<SentimentVerySatisfiedRoundedIcon />}
                 label={task.priority}
-                variant="outlined"
-                color="info"
-                size="small"
+                variant='outlined'
+                color='info'
+                size='small'
               />
             )}
             {task.priority == Priority.MEDIUM && (
               <Chip
                 icon={<SentimentNeutralRoundedIcon />}
                 label={task.priority}
-                variant="outlined"
-                color="warning"
-                size="small"
+                variant='outlined'
+                color='warning'
+                size='small'
               />
             )}
             {task.priority == Priority.HIGH && (
               <Chip
                 icon={<MoodBadRoundedIcon />}
                 label={task.priority}
-                variant="outlined"
-                color="error"
-                size="small"
+                variant='outlined'
+                color='error'
+                size='small'
               />
             )}
           </Box>
         </Box>
         {task.description && (
-          <Box width="100%" display="flex" flexDirection="column" gap="5px">
-            <TaskCollapseFieldTitle variant="h6" component="h5">
-              <DescriptionRoundedIcon fontSize="small" />
+          <Box width='100%' display='flex' flexDirection='column' gap='5px'>
+            <TaskCollapseFieldTitle variant='h6' component='h5'>
+              <DescriptionRoundedIcon fontSize='small' />
               Description:
             </TaskCollapseFieldTitle>
-            <Typography variant="body1" component="p">
+            <Typography variant='body1' component='p' sx={{ wordWrap: "break-word" }}>
               {task.description}
             </Typography>
           </Box>
